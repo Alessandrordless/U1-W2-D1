b6a127b51b588fc584b113c4308cb19101e3f4ec
Alessandrordless <alessandronicoletti1@gmail.com>
2023-10-02 16:44:17 +0200

    commit():WithOutAStep

--- asset/D1.js
+++ asset/D1.js
@@ -8,7 +8,7 @@ REGOLE
 */
 
 /* ESERCIZIO 1
- Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 const string = `stringa`;
 console.log(`questo è un dato di tipo stringa ("parole  o frasi") :` + ` ` + string);
@@ -28,18 +28,18 @@ const entity = `simbolo`;
 console.log(`Questo è un datatype di tipo Symbol  :` + `` + entity);
 
 /* ESERCIZIO 2
- Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 let name = `Alessandro`;
 console.log(name);
 
 /* ESERCIZIO 3
- Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 console.log(number + 20);
 
 /* ESERCIZIO 4
- Crea una variable di nome "x" e assegna ad essa il numero 12.
+  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 const x = 12;
 console.log(x);
@@ -47,22 +47,21 @@ console.log(x);
 // Esercizio 5 spostato a fine pagine per motivi di carattere tecnico
 
 /* ESERCIZIO 6
- Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 console.log(4 - x);
 
 /* ESERCIZIO 7
- Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
- Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
- EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
 let name1 = "john";
 let name2 = "John";
 console.log(name1 !== name2);
 
-let result = name2.toLowerCase();
-console.log(name1 !== name2);
+console.log(name1 === name2.toLowerCase());
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
