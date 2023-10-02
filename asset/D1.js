787600bca981e599dafe1f6f01eafc8404e96e9c
Alessandrordless <alessandronicoletti1@gmail.com>
2023-10-02 17:59:59 +0200

    commit()apici

--- asset/D1.js
+++ asset/D1.js
@@ -10,33 +10,37 @@ REGOLE
 /* ESERCIZIO 1
   Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
-const string = `stringa`;
-console.log(`questo è un dato di tipo stringa ("parole  o frasi") :` + ` ` + string);
+const string = "stringa";
+console.log("questo è un dato di tipo stringa(parole o frasi) :" + " " + string);
 
 const number = 12;
-console.log(`questo è un dato di tipo number (17) :` + ` ` + number);
+console.log("questo è un dato di tipo number (17) :" + " " + number);
 
-const boolean = "true or false";
-console.log(`Questo è un datatype boolean (vero o falso) :` + boolean);
+const boolean = true;
+const boolean2 = false;
+console.log("Questo è un datatype boolean (vero o falso) :" + boolean + " " + boolean2);
 
-console.log(`Questo è un datatype UNDEFINED, all'interno non è stato ancora assegnato un valore`);
+let empty;
+
+console.log("Questo è un datatype" + " " + "all'interno non è stato ancora assegnato un valore :" + " " + empty);
 
 const not = null;
-console.log(`Questo è un datatype di tipo nullo (intenzionalmente lasciato vuoto) :` + `` + null);
+console.log("Questo è un datatype di tipo nullo (intenzionalmente lasciato vuoto) :" + " " + null);
 
-const entity = `simbolo`;
-console.log(`Questo è un datatype di tipo Symbol  :` + `` + entity);
+const entity = "simbolo";
+console.log("Questo è un datatype di tipo Symbol  :" + " " + entity);
 
 /* ESERCIZIO 2
   Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
-let name = `Alessandro`;
+let name = "Alessandro";
 console.log(name);
 
 /* ESERCIZIO 3
   Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
-console.log(number + 20);
+let numero5 = 20;
+console.log(number + numero5);
 
 /* ESERCIZIO 4
   Crea una variable di nome "x" e assegna ad essa il numero 12.
@@ -49,6 +53,7 @@ console.log(x);
 /* ESERCIZIO 6
   Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
+
 console.log(4 - x);
 
 /* ESERCIZIO 7
@@ -61,16 +66,16 @@ let name1 = "john";
 let name2 = "John";
 console.log(name1 !== name2);
 
-console.log(name1 === name2.toLowerCase());
+console.log(name1.toLowerCase() === name2.toLowerCase());
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
-name = `Nicoletti`;
+name = "Nicoletti";
 console.log(name);
 
-const rule1 = `invariabile`;
-rule1 = `variabile`;
+const rule1 = "invariabile";
+rule1 = "variabile";
 console.log(rule1);
 //il valore della variabile costante non può essere alterato guarda errore console
