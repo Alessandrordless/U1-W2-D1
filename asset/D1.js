e78f94ea04bf1a5d70635f5d904251e374434100
Alessandrordless <alessandronicoletti1@gmail.com>
2023-10-05 22:24:02 +0200

    commit

--- asset/D1.js
+++ asset/D1.js
@@ -30,51 +30,40 @@ const entity = "simbolo";
 console.log("Questo è un datatype di tipo Symbol  :" + " " + entity);
 
 /* ESERCIZIO 2
-  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
-let name = "Alessandro";
-console.log(name);
+let myName = "Ale";
 
 /* ESERCIZIO 3
-  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
-let numero5 = 20;
-console.log(number + numero5);
+console.log(10 + 7);
 
 /* ESERCIZIO 4
-  Crea una variable di nome "x" e assegna ad essa il numero 12.
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
-const x = 12;
-console.log(x);
-
-// Esercizio 5 spostato a fine pagine per motivi di carattere tecnico
-
-/* ESERCIZIO 6
-  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
-*/
-
-console.log(4 - x);
-
-/* ESERCIZIO 7
-  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
-  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
-  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
-*/
-
-let name1 = "john";
-let name2 = "John";
-console.log(name1 !== name2);
-
-console.log(name1.toLowerCase() === name2.toLowerCase());
+let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
-name = "Nicoletti";
-console.log(name);
+myName = "Nicoletti";
 
-const rule1 = "invariabile";
-rule1 = "variabile";
-console.log(rule1);
-//il valore della variabile costante non può essere alterato guarda errore console
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+let subtraction = 4 - x;
+console.log(subtraction);
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+let name1 = "john";
+let name2 = "John";
+console.log(name1 !== name2);
+console.log(name1.toLowerCase() === name2.toLowerCase());
